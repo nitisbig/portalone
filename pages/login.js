0361c487e09ecb851af284e89cc58fcd95a17517
nitisbig <115392360+nitisbig@users.noreply.github.com>
2025-09-06 07:42:27 +0545

    Add Google sign-in option

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import { Container, TextField, Button, Box, Typography, Link } from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
 import NextLink from 'next/link';
 
 export default function Login() {
@@ -11,6 +12,9 @@ export default function Login() {
           <TextField margin="normal" required fullWidth name="password" label="Password" type="password" autoComplete="current-password" />
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
             Sign In
+          </Button>
+          <Button fullWidth variant="contained" color="primary" startIcon={<GoogleIcon />} sx={{ mb: 2 }}>
+            Continue with Google
           </Button>
           <Link component={NextLink} href="/signup" variant="body2">
             {"Don't have an account? Sign Up"}
